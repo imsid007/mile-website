302fec2985bfe4de7d555c56a29f04d25e970861
imsid1996 <48718441+imsid007@users.noreply.github.com>
2022-01-21 12:04:31 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "antd/dist/antd.css";
+import "../styles/globals.css";
+import "../styles/bootstrap.min.css";
+import "../styles/fontAwesome.css";
+import "../styles/hero-slider.css";
+import "../styles/owl-carousel.css";
+import "../styles/templatemo-main.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
